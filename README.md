7eaa9f16c56136862a40759ab0543ca2f885d925
stoiant <stoiant@gmail.com>
2019-03-10 23:24:36 -0400

    Added full handling of GET requests, including paramters and using client/server headers, no support for chuncking or sockets

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ## Goal
 
-The goal is to write a web service that acts as an `HTTP(S)`   proxy in python. 
+The goal is to write a web service that acts as an `HTTP(S)` proxy in python. 
+
+If you know what you are doing it will take about 42 minutes.
 
 ## Requirements
 
@@ -14,11 +16,55 @@ It should listen for both `GET` and `POST`
 * On a `POST` request, it should make a post request to `<url>`, passing through any form data
 It should set the user-agent header to the same user-agent that it is being called by (note `curl/7.35.0` in the examples)
 
+## Installation
+
+### Requirements
+
+* python3
+* pip3
+* external libraries(packages)
+ * simplejson
+ * requests
+
+### Steps to setup environment
+
+* clone repository
+* ensure python3 and pip3 is installed
+* setup virtaul environment to download packages
+
+```
+virtualenv venvdev
+virtualenv -p /usr/local/bin/python3 venvdev
+source venv/bin/activate
+source venvdev/bin/activate
+pip3 install requests
+pip3 install simplejson
+```
+
+### Run the server
+
+Ports are defined as 8001 for `HTTP` and 8002 for `HTTPS` if ports are busy on the system running the proxy server then code will have to be modified accordingly.
+
+For simplicity ports are not taken as input paramters.
+
+```
+python3 server.py
+```
+
 ## Limitations
 
 * single user
 * performance is not relevant
 * no support for Websockets
+* no support for chunking
+* limited response as it is written at once
+* running on port 8001(HTTP) and 8002(HTTPS)
+* relative url requests will fail due to /proxy/ not being used (e.g. viewing a website)
+* logging is for debug prupposes
+* no TLS
+* response encoding set to `UTF-8` for custom messages
+* REQUEST_TIMEOUT set to 10 seconds
+* timeboxed fun to toy with :)
 
 ## SSL Self Signed Certificate
 
@@ -27,9 +73,43 @@ It should set the user-agent header to the same user-agent that it is being call
 openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365
 ```
 
-Note: To server SSL requests the certificate will have to be trusted by the local server.
+### Remove passphrase to avoid manual entry
+```
+openssl rsa -in [key.pem] -out [key_no_pass.pem]
+```
 
-## Examples 
+Note: To properly server SSL requests the certificate will have to be trusted by the local server.
+
+## Testing
+
+### Dev Environment
+
+MacOS
+
+IDE (VisualStudio)
+
+### Display basic web-pages
+
+Verify sites such as http://google.ca https://google.ca render with the provided limitations.
+
+<https://127.0.0.1:8002/proxy/https://gotrain.app>
+<http://127.0.0.1:8001/proxy/https://google.ca>
+
+### Use Postman
+
+-  test to ensure handling of `HTTP` requests and proper message is 	displayed if request is not supported
+- `RFC 7231`, section 4: Request methods	Hypertext Transfer Protocol (HTTP/1.1): Semantics and Content	Specifies GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE.
+
+### REST Calls
+
+The following site was utilized for `GET` `POST` API calls:
+
+<https://reqres.in/>
+
+<http://127.0.0.1:8001/proxy/https://reqres.in/api/users?page=2>
+
+
+## Provided Examples 
 Assuming the web service is running on localhost:8000, here are some examples of how it can be used:
 
 * `GET`
@@ -75,3 +155,13 @@ $ curl -X POST -d asdf=blah  http://localhost:8000/proxy/http://httpbin.org/post
   "url": "http://httpbin.org/post"
 }
 ```
+
+## References
+
+<https://docs.python.org/3/library/urllib.parse.html>
+<https://docs.python.org/3/library/http.server.html>
+<https://docs.python.org/3/library/http.html>
+<https://realpython.com/python-requests/#the-get-request>
+<https://docs.python.org/3.3/library/http.client.html>
+<https://docs.python.org/3/library/io.html>
+<https://realpython.com>
--- server.py
+++ server.py
@@ -1,44 +1,161 @@
 #!/usr/bin/env python3
 import logging, cgi, ssl, re
 from urllib.parse import urlparse
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib
+from http.server import HTTPServer, BaseHTTPRequestHandler, HTTPStatus
 import time, sys
 from threading import Thread
+#requires pip install
+import requests
+from requests.exceptions import HTTPError
+from requests.exceptions import Timeout
+#requires pip install
+import simplejson as json
+import http.client
+import io
 
 #define port to be used for the server
 HTTP_PORT = 8001
 HTTPS_PORT = 8002
 ENCODING = 'UTF-8'
+REQUEST_TIMEOUT = 10
 
 logging.getLogger().setLevel(logging.INFO)
 
 class GetPostServer(BaseHTTPRequestHandler):
+    def do_DELETE(self):
+        logging.info(self.headers)
+        self.send_response(HTTPStatus.NOT_IMPLEMENTED.value)
+        self.end_headers()
+        message = HTTPStatus.NOT_IMPLEMENTED.description
+        logging.error(message)
+        self.wfile.write(bytes(message, ENCODING))
     def do_GET(self):
         logging.info(self.headers)
+
+        #delete does not seem to return not implemented so adding it to the proxy
+        if(self.command == 'DELETE'):
+            self.do_DELETE
 
         #ensure that only the verb proxy is processed
         target = self.parse_path_for_proxy_verb(self.path)
         if(target is None):
-            self.send_response(400)
-            self.end_headers()
-            message = "Bad proxy URL for GET of  '{}' detected.".format(self.path)
+            self.send_response(HTTPStatus.BAD_REQUEST)
+            BaseHTTPRequestHandler.end_headers(self)
+            message = "Bad proxy URL for GET of '{}' detected.".format(self.path)
             logging.error(message)
             self.wfile.write(bytes(message, ENCODING))
         else:
-            self.send_response(200)
-            self.end_headers()
             message = "Detected GET request for '{}'".format(target)
             logging.info(message)
-            self.wfile.write(bytes(message, ENCODING))
-            #SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
- 
+
+            dHeaders = self.get_destination_headers()
+            dParams = self.get_destination_params(target)
+
+            response = None
+            try:
+                response = requests.get(target, headers=dHeaders, params=dParams, timeout=REQUEST_TIMEOUT)
+                logging.info("Received response code of '{}' from '{}'".format(response.status_code, target))
+
+                # If the response was successful, no Exception will be raised
+                response.raise_for_status()
+
+            except HTTPError as http_err:
+                message = "HTTP error occurred: {}".format(http_err)
+                logging.error(message)
+                if(response != None and response.status_code != None):
+                    self.send_response(response.status_code)
+                else:
+                    self.send_response(HTTPStatus.INTERNAL_SERVER_ERROR)
+
+                self.end_headers()
+
+                if(response != None and response.status_code != None):
+                    self.wfile.write(response.content)
+                else:
+                    self.wfile.write(bytes(message, ENCODING))
+
+            except Timeout:
+                message = "The request timed out in {}s".format(REQUEST_TIMEOUT)
+                logging.error(message)
+                if(response != None and response.status_code != None):
+                    self.send_response(response.status_code)
+                else:
+                    self.send_response(HTTPStatus.REQUEST_TIMEOUT)
+
+                self.end_headers()
+
+                if(response != None and response.status_code != None):
+                    self.wfile.write(response.content)
+                else:
+                    self.wfile.write(bytes(message, ENCODING))
+            except Exception as err:
+                message = "Other error occurred: {}".format(err)
+                logging.error(message)
+                if(response != None and response.status_code != None):
+                    self.send_response(response.status_code)
+                else:
+                    self.send_response(HTTPStatus.INTERNAL_SERVER_ERROR)
+
+                self.end_headers()
+
+                if(response != None and response.status_code != None):
+                    self.wfile.write(response.content)
+                else:
+                    self.wfile.write(bytes(message, ENCODING))                
+            else:
+                logging.info("Call to target host successful.")
+                self.send_response(response.status_code)
+                self.write_headers(response.headers)
+                self.wfile.write(response.content)
+                self.close_connection = True
+
+    def get_destination_params(self, target):
+        params = {}
+        if(self.is_url(target)):
+            logging.info(urllib.parse.urlsplit(target))
+            logging.info(urllib.parse.parse_qs(urllib.parse.urlsplit(target).query))
+            params = dict(urllib.parse.parse_qsl(urllib.parse.urlsplit(target).query))
+
+        return params
+
+    def get_destination_headers(self):
+        dHeaders = {}
+        lines = io.StringIO(str(self.headers))
+        line = lines.readline()
+        while line:
+            if line.strip() != '':
+                print(line.strip())
+                match = re.search('^((?P<key>\S+):\s*(?P<value>.*))$', line.strip())
+                if match is None:
+                    logging.error("Invalid header line format of '{}' detected. Ignoring header.".format(line.strip()))
+                else:
+                    key = match.groups()[1]
+                    value = match.groups()[2]
+                    if(key.lower() != 'Host'.lower()):
+                        dHeaders[key] = value
+
+            line = lines.readline()
+        return dHeaders
+
+    def write_headers(self, headers):
+        for key, value in headers.items():
+            logging.info("{}:{}".format(key,value))
+            #serving the raw response with no support for chunking
+            if(key.lower() == 'Transfer-Encoding'.lower()):
+                logging.info("Skipping {} with value {}".format(key,value))
+            else:
+                self.send_header(key, value)
+    
+        self.end_headers()
+
     def do_POST(self):
         logging.info(self.headers)
 
         #ensure that only the verb proxy is processed
         target = self.parse_path_for_proxy_verb(self.path)
         if(target is None):
-            self.send_response(400)
+            self.send_response(HTTPStatus.BAD_REQUEST)
             self.end_headers()
             message = "Bad proxy URL for POST of  '{}' detected.".format(self.path)
             logging.error(message)
@@ -53,7 +170,7 @@ class GetPostServer(BaseHTTPRequestHandler):
                 environ={'REQUEST_METHOD':'POST',
                         'CONTENT_TYPE':self.headers['Content-Type'],
                         })
-            self.send_response(200)
+            self.send_response(HTTPStatus.OK)
             self.end_headers()
             for item in form.list:
                 logging.info(item)
@@ -102,7 +219,7 @@ if __name__ == '__main__':
     try:
         init_server("", HTTPS_PORT, True)
         init_server("", HTTP_PORT, False)
-        input('Press enter to stop servers.')
+        input('Press enter to stop toy proxy servers.')
     except KeyboardInterrupt:
-        print("Ctrl C - Stopping server")
+        print("Ctrl C - Stopping toy proxy server")
         sys.exit(1)
